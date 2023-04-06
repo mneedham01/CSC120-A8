fa02af325a9fc8ee5bffb5166c470a3468b114f3
mneedham01 <mneedham@smith.edu>
2023-04-06 11:47:38 -0400

    added empty methods

---
+++ Raven.java
@@ -0,0 +1,43 @@
+
+public class Raven implements Contract.java{
+
+    String holdingInMyClaws="";
+    String lookingAtWithBeadyEyes="";
+
+    public void grab(String item){
+
+
+    }
+
+    public String drop(String item){
+
+    }
+
+    public void examine(String item){
+
+    }
+
+    public void use(String item){
+
+    }
+
+    public boolean walk(String direction){
+
+    }
+
+    public boolean fly(int x, int y){
+
+    }
+
+    public Number shrink(){
+
+    }
+
+    public void rest(){
+
+    }
+
+    public void undo(){
+
+    }
+}
