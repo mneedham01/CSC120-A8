97e551da314f0cd3497d28d2ada642b98db89ce7
mneedham01 <mneedham@smith.edu>
2023-04-10 20:49:43 -0400

    put in testing for everything

--- Raven.java
+++ Raven.java
@@ -36,22 +36,27 @@ public class Raven implements Contract.java
         this.holdingWithMyClaws=true;
         this.itemThatIsMINE=item;
         System.out.println("You just picked up "+item+ " in your powerful claws.");
-
+        this.deservedlyResting=false;
     }
 
     public String drop(String item){
         if (!this.holdingWithMyClaws){
-            throw new RuntimeException("You are not holidng anything in your claws. Grab something in order to drop it.");
+            throw new RuntimeException("You are not holding anything in your claws. Grab something in order to drop it.");
+        }
+        if (!this.itemThatIsMINE.equals(item)){
+            throw new RuntimeException("You are not holding a "+item+" so you cannot drop it.");
         }
         this.holdingWithMyClaws=false;
         this.busyUsingMyItem=false;
         this.itemThatIsMINE="";
+        this.deservedlyResting=false;
         System.out.println("You just dropped "+ item+ " from your powerful claws.");
         return item;
     }
 
     public void examine(String item){
         this.lookingAtWithBeadyEyes=item; 
+        this.deservedlyResting=false;
         System.out.println("You are examining "+item);
     }
 
@@ -63,38 +68,55 @@ public class Raven implements Contract.java
             throw new RuntimeException("You are not holding anything in your claws. Grab something in order to use it.");
         }
         if(!item.equals(this.itemThatIsMINE)){
-            throw new RuntimeException("You are not holding "+item+" in your claws, so you cannot use it. Grab it first in order to use it.");
+            throw new RuntimeException("You are not holding a "+item+" in your claws, so you cannot use it. Grab it first in order to use it.");
         }
         this.busyUsingMyItem=true;
-        System.out.println("You are currently using "+item+", as you should. Undo in order to stop using it.");
+        this.deservedlyResting=false;
+        System.out.println("You are currently using "+item+", as you should.");
         
     }
 
     public boolean walk(String direction){
-        if (direction!= "LEFT" ||direction!="RIGHT"||direction!="FORWARD"||direction!="BACKWARD"){
+        if (direction!= "LEFT" &&direction!="RIGHT"&&direction!="FORWARD"&&direction!="BACKWARD"){
             throw new RuntimeException("The only valid directions a raven can walk are 'LEFT','RIGHT','FORWARD', or 'BACKWARD'.");
         }
         if (direction=="LEFT"){
+            if (xPosition<5){
+                throw new RuntimeException("You cannot walk left as you are near the border of your area. Try walking right.");
+            }
             this.previousXPosition=this.xPosition;
             this.xPosition -=5;
+            this.deservedlyResting=false;
             System.out.println("You are strutting proudly to your new destination: ("+this.xPosition+" miles, "+this.yPosition+" miles).");
             return true; 
         }
         if (direction=="RIGHT"){
+            if (xPosition>95){
+                throw new RuntimeException("You cannot walk right as you are near the border of your area. Try walking left.");
+            }
             this.previousXPosition=this.xPosition;
             this.xPosition +=5;
+            this.deservedlyResting=false;
             System.out.println("You are strutting proudly to your new destination: "+this.xPosition+" miles, "+this.yPosition+" miles).");
             return true;
         }
         if (direction=="FORWARD"){
+            if (yPosition>95){
+                throw new RuntimeException("You cannot walk forward as you are near the border of your area. Try walking backward.");
+            }
             this.previousYPosition=this.yPosition;
             this.yPosition+=5;
+            this.deservedlyResting=false;
             System.out.println("You are strutting proudly to your new destination: ("+this.xPosition+" miles, "+this.yPosition+" miles).");
             return true;
         }
         if (direction=="BACKWARD"){
+            if (yPosition<5){
+                throw new RuntimeException("You cannot walk backward as you are near the border of your area. Try walking forward.");
+            }
             this.previousYPosition=this.yPosition;
             this.yPosition-=5;
+            this.deservedlyResting=false;
             System.out.println("You are strutting proudly to your new destination: ("+this.xPosition+" miles, "+this.yPosition+" miles).");
             return true; 
         }
@@ -105,15 +127,22 @@ public class Raven implements Contract.java
         if(this.wingsIn){
             throw new RuntimeException("Your wings are in, so you cannot fly. Grow, and then try again.");
         }
+        if(x>100||y>100){
+            throw new RuntimeException("This location is out of bounds. Choose a new location.");
+        }
         this.previousXPosition=this.xPosition;
         this.previousYPosition=this.yPosition;
         this.xPosition=x;
         this.yPosition=y;
-        System.out.println("You have flown majestically to a new destination: ("+this.xPosition+", "+this.yPosition+").");
+        this.deservedlyResting=false;
+        System.out.println("You have flown majestically to a new destination: ("+this.xPosition+" miles, "+this.yPosition+" miles).");
         return true;
     }
 
     public Number shrink(){
+        if(this.wingsIn){
+            throw new RuntimeException("Your wings are already folded in.");
+        }
         this.wingsIn=true;
         System.out.println("Your beautiful wings are folded in and you're ready to rest.");
         int number = 4;
@@ -122,7 +151,11 @@ public class Raven implements Contract.java
     }
 
     public void grow(){
+        if(!this.wingsIn){
+            throw new RuntimeException("Your wings are already splayed.");
+        }
         this.wingsIn=false;
+        this.deservedlyResting=false;
         System.out.println("You wings are splayed and you're ready to fly.");
     }
 
@@ -131,12 +164,14 @@ public class Raven implements Contract.java
             throw new RuntimeException("Your wings are splayed. Shrink in order to tuck in your wings in order to rest.");
         }
         this.deservedlyResting=true;
+        System.out.println("You are taking a deserved rest.");
     }
 
     public void undo(){
         System.out.println("The undo funtion brings you back to where you were before you last walked or flew.");
         this.xPosition=this.previousXPosition;
         this.yPosition=this.previousYPosition;
+        this.deservedlyResting=false;
         System.out.println("You are now back at ("+this.xPosition+", "+this.yPosition+").");
     }
 
@@ -174,6 +209,150 @@ public class Raven implements Contract.java
     public static void main(String args[]){
         Raven Harold=new Raven("Harold");
         System.out.println(Harold);
+        System.out.println("\n");
+        //testing grabbing an item 
+        Harold.grab("twine");
+        System.out.println(Harold);
+        System.out.println("\n");
+        //testing grabing another item 
+        try{
+            Harold.grab("nickel");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        //testing dropping an item 
+        Harold.drop("twine");
+        System.out.println("\n");
+        //testing dropping an item while I am not holding anything 
+        try{
+            Harold.drop("twine");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        //testing dropping an item I am not holding 
+        Harold.grab("feather");
+        try{
+            Harold.drop("pencil");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        //testing examining an item 
+        Harold.examine("human");
+        System.out.println("\n");
+        System.out.println(Harold);
+        System.out.println("\n");
+        //testing using an item 
+        Harold.use("feather");
+        System.out.println("\n");
+        //testing using an item while I'm already using an item 
+        try{
+            Harold.use("penny");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        //testing using an item while I'm not holding anything in my claws 
+        Harold.drop("feather");
+        System.out.println("\n");
+        try{
+            Harold.use("penny");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        //testing using an item I'm not holding that item in my claws
+        Harold.grab("feather");
+        System.out.println("\n");
+        try{
+            Harold.use("penny");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        //testing walking 
+        Harold.walk("LEFT");
+        System.out.println("\n");
+        Harold.walk("RIGHT");
+        System.out.println("\n");
+        Harold.walk("FORWARD");
+        System.out.println("\n");
+        Harold.walk("BACKWARD");
+        System.out.println("\n");
+        //test undo 
+        Harold.undo();
+        System.out.println("\n");
+        //testing flying 
+        Harold.fly(98,98);
+        System.out.println("\n");
+        //testing flying out of bounds 
+        try{
+            Harold.fly(150,90);
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        //testing walking out of bounds 
+        try{
+            Harold.walk("FORWARD");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        try{
+            Harold.walk("RIGHT");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        Harold.fly(2,2);
+        try{
+            Harold.walk("BACKWARD");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        try{
+            Harold.walk("LEFT");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        //testing shrinking 
+        Harold.shrink();
+        System.out.println("\n");
+        //testing shrinking when already shrunk 
+        try{
+            Harold.shrink();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        //testing resting 
+        Harold.rest();
+        System.out.println("\n");
+        //testing resting when wings are splayed 
+        Harold.grow();
+        try{
+            Harold.rest();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\n");
+        //testing growing when wings are splayed 
+        try{
+            Harold.grow();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+        
+
+
+
+
 
     }
 }
